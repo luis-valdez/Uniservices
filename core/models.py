20d22f15d57526bd8649f0098593d31fcbefbdbe
Emilio Wong <emilews.ew@gmail.com>
2018-12-02 18:28:50 -0700

    Fixed logged in messages and options

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 
 class Campus(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.PositiveSmallIntegerField(primary_key=True)
     nombre = models.CharField(max_length=100)
     imagen = models.ImageField(null=False, blank=False)
 
@@ -13,7 +13,7 @@ class Campus(models.Model):
 	    return self.nombre
 
 class Division(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.PositiveSmallIntegerField(primary_key=True)
     campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=100)
     imagen = models.ImageField(null=False, blank=False)
