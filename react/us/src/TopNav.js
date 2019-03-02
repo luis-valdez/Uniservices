ab2524c53b38d6b5bc097eb1d293ba7ccb75c65b
Emilio Wong <emilews.ew@gmail.com>
2019-03-01 23:05:27 -0700

    Deleted 'full calendar' and created base react project

---
+++ react/us/src/TopNav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './TopNav.css';
+
+export default class TopNav extends Component{
+    render(){
+        return(
+            <div className="Top">
+
+            </div>
+        )
+    }
+
+}
